046c21f33a4219db4b5d2ea35d38d1abcc4ded49
pdrake1988 <pdrake1@me.com>
2020-12-29 00:41:24 -0600

    Modified MovieController.cs and view

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using pdrake.Models.MovieProject;
 
@@ -6,8 +10,24 @@ namespace pdrake.Controllers
 {
     public class MovieController : Controller
     {
-        public async Task<MovieResults> GetMovies()
+        public async Task<ActionResult> GetMovies(int genreId = 0, int page = 1)
         {
+            using HttpClient httpClient = new HttpClient();
+            if (genreId == 0)
+            {
+                var apiResponse = await httpClient.GetStreamAsync(
+                    "https://api.themoviedb.org/3/discover/movie?api_key=e2e4f004450c3b2d09d61c0fb5120d06&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=true&page=" + page);
+                ViewData["movies"] = await JsonSerializer.DeserializeAsync<IEnumerable<MovieResults>>(apiResponse);
+                IEnumerable<MovieResults> movieResults = ViewData["movies"] as IEnumerable<MovieResults>;
+            }
+            else
+            {
+                var apiResponse = await httpClient.GetStreamAsync(
+                    "https://api.themoviedb.org/3/discover/movie?api_key=e2e4f004450c3b2d09d61c0fb5120d06&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=true&" + "with_genre=" + genreId + "&page=" + page);
+                ViewData["movies"] = await JsonSerializer.DeserializeAsync<IEnumerable<MovieResults>>(apiResponse);
+                IEnumerable<MovieResults> movieResults = ViewData["movies"] as IEnumerable<MovieResults>;
+            }
+
             return View();
         }
     }
